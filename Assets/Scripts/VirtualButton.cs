2a85987e3e1566c862d34f2742b5f578efc4421a
ElRMapache <alo15470@uvg.edu.gt>
2018-02-21 09:09:25 -0600

    todo menos apk

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+ 
+    }
+
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+
+    // Use this for initialization
+    void Start (){
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
